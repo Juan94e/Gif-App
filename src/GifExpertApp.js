7a5f06070b72042e91172f78c86691a2c17f64f2
Juan E <juan94e@gmail.com>
2021-09-17 09:07:17 -0500

    new button added. just atrting with the search button

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -2,16 +2,14 @@ import React, { useState } from 'react'
 import './index.css';
 import { Switch, Grid, Typography, Button, Paper } from '@material-ui/core';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-import { height } from '@mui/system';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const GifExpertApp = () => {
-
-    const categories = ['Dragon Ball', 'One Punch', 'Samurai X'];
 
     const [darkMode, setDarkMode] = useState(false);
 
     const theme = createTheme ({
-        palette: {
+        palette: { 
             type: darkMode ? "dark" : "light",
         },
     });
@@ -20,12 +18,29 @@ export const GifExpertApp = () => {
         setDarkMode(!darkMode)
     }
 
+    
+    const [categories, setCategories] = useState(['en componentes el dark mode etc', 'mirar lo del shadow y color en el boton para que sea blanco', 'mañana viernes termino'])
+    
+    // setCategories toma el valor del estado anterior y lo guarda en "categs" y devuelve ese nuevo Array(valor)
+    const handleAdd = () => {
+        setCategories( (categs) => [...categs, 'otra mas'] )
+    }
+
     return (
         <ThemeProvider theme={theme}>
-            <Paper style={{height: "100vh"}} elevation={2} square >
-                <Grid container direction="column" >
+            <Paper style={{height: "100vh", padding:"15px"}} elevation={2} square >
+                
                 <h2>GifExpertApp</h2>
                 {/* <hr /> */}
+                <Button 
+                    size="large"
+                    color="primary"
+                    variant="contained"
+                    startIcon={<SearchIcon />}
+                    onClick={ handleAdd }
+                >
+                    Search
+                </Button>
                 <ol>
                     {
                         categories.map(category => {
@@ -33,8 +48,8 @@ export const GifExpertApp = () => {
                         })
                     }
                 </ol>
-                    <Switch checked={darkMode} onChange={ handleSwitch } />
-                </Grid>
+                    <Switch color="primary" checked={darkMode} onChange={ handleSwitch } />
+                
             </Paper>
         </ThemeProvider>
     )
