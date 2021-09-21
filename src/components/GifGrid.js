22edfd9b91f3470dc42bc1d8f0597e835661268a
Juan E <juan94e@gmail.com>
2021-09-21 10:17:45 -0500

    divido con el helper y quito comentario importante

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGIfs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category} ) => {
@@ -6,26 +7,27 @@ export const GifGrid = ( {category} ) => {
     const [images, setImages] = useState([]);
 
     useEffect( () => {
-        getGifs();
+        getGifs( category )
+            .then( imgs => setImages( imgs ) )
     }, [])
 
-    const getGifs = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=M6hqmV7sKXm49MA7xT5ayne2tndKJFhj`
-        const resp = await fetch(url);
-        const { data } = await resp.json();
+    // const getGifs = async() => {
+    //     const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=M6hqmV7sKXm49MA7xT5ayne2tndKJFhj`
+    //     const resp = await fetch(url);
+    //     const { data } = await resp.json();
 
-        const gifs = data.map( (img) => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-        //con la instruccion de abajo manda la constante "gifs" que se encuentra
-        //dentro de una const (no es global), entonces no se puede llamar afuera
-        //pero con ese "setImages" mandamos la informacion a una funcion global
-        setImages( gifs )
-    }
+    //     const gifs = data.map( (img) => {
+    //         return {
+    //             id: img.id,
+    //             title: img.title,
+    //             url: img.images?.downsized_medium.url
+    //         }
+    //     })
+    //     //con la instruccion de abajo manda la constante "gifs" que se encuentra
+    //     //dentro de una const (no es global), entonces no se puede llamar afuera
+    //     //pero con ese "setImages" mandamos la informacion a una funcion global
+    //     setImages( gifs )
+    // }
 
     return (
         <div>
