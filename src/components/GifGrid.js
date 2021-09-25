ec6ec80ec083e206cbd27f7501749087d6926136
Juan E <juan94e@gmail.com>
2021-09-24 20:43:33 -0500

    termino la paginación, sin embargo la apis olo me manda 50 por mas que cambie el limit

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -2,26 +2,57 @@ import React, { useState, useEffect } from 'react'
 import { getGifs } from '../helpers/getGIfs';
 import { GifGridItem } from './GifGridItem';
 import Typography from '@mui/material/Typography';
+import '../styles/GifGrid.css';
+
+// en este componenete estamos ocnstruyendo la paginacion con el filtrado de los gifs que llegan
 
 export const GifGrid = ( { category, darkMode } ) => {
 
     const [images, setImages] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
 
     useEffect( () => {
         getGifs( category )
             .then( imgs => setImages( imgs ) )
     }, [ category ])
 
-    const filtrado =  images.slice(0, 5);
+    // const filtrado =  images.slice(0, 2);
+
+    const filtrado = () => {
+        return  images.slice(currentPage, currentPage + 9)
+    }
+
+    const previousPage  = () => {
+        if ( currentPage > 0 )
+            setCurrentPage( currentPage - 9 )
+    }
+
+    const nextPage  = () => {
+        setCurrentPage( currentPage + 9 )
+    }
     
 
     return (
         <div >
+            <button
+                className={ `shadow-sm btn mt-3 bf ${ darkMode ? "af" : null} } `} 
+                onClick={ previousPage }
+            > 
+                Previous
+            </button>
+
+            <button
+                className={ `shadow-sm btn mt-3 ms-3 bf ${ darkMode ? "af" : null} } `} 
+                onClick={ nextPage }
+            >
+                Next
+            </button>
+
             <Typography id="category-capitalize" variant="h2"> { category } </Typography>
             <div className="card-grid" >
 
                 {
-                    filtrado.map( (img) => {
+                    filtrado().map( (img) => {
                         return <GifGridItem
                             key={ img.id }
                             img = { img }
--- src/styles/customTheme.js
+++ src/styles/customTheme.js
@@ -4,13 +4,16 @@ import { green, red,purple, blue } from '@mui/material/colors';
 export const lightTheme = createTheme ({
     palette: {
         type:"light",
+        primary: green,
         secondary: blue,
+
     },
 });
 
 export const darkTheme = createTheme ({
     palette: {
         type:"dark",
+        primary: green,
         secondary: green,
         background: {
             default: "#212529",
